b5a6b8ad626d38213f6a076ba2542ba29b9f77ac
Douglass Coronado <dcoronado@funio.com>
2023-10-04 20:28:36 -0400

    add source image reference

--- windows-vm.tf
+++ windows-vm.tf
@@ -29,8 +29,8 @@ resource "azurerm_windows_virtual_machine" "myvm" {
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
   location            = azurerm_resource_group.azureresourcegroup.location
   size                = "Standard_F2"
-  admin_username      = "${var.admin_username}"
-  admin_password      = "${var.admin_password}"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
   network_interface_ids = [
     azurerm_network_interface.mynic.id,
   ]
@@ -38,6 +38,13 @@ resource "azurerm_windows_virtual_machine" "myvm" {
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+  }
+  
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
   }
 
 }
